f68741b558b7e3ec705914dbbc600ba281a0a4e9
Thomas Whitelock <trnthomas@live.com>
2018-10-01 21:07:37 -0400

    updated index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>Introduction</title>
+</head>
+<body>
 
-  phpinfo();
+<h1>About Me</h1
+<p>My name is Thomas. I actually do like to play Pokémon. I bought Diamond and Pearl for the DS when they first came out. I bought them for myself and my 2 sons. We played it a lot, hundreds of hours. On occasion my wife would let them buy packs of Pokémon cards at Walmart. The ammased quite a collection of cards over time. My wife hurt her neck and had to sleep in a recliner for a few months.</p>
 
-?>
+<p>So as not to bother her and let her rest in peace, I suggested that we try and learn of tp play the card game. We did learn, but we were terrible. One day I looked online and noticed that the Pokémon World Championships were going to be played in Orlando a few weeks from then. So we decided to go and see what that was like. We became addicted to the game. I ended up running a league for 6 years and my sons had two invites to the Pokémon World Chapionships.We stopped playing in 2014, the game format was stale and my oldest son was leaving on his mission and we were tired or traveling to tournaments all the time!</p>
+
+</body>
+</html>
