d2274d288e78e0f15281dc60248502d6084f3ecf
Thomas Whitelock <trnthomas@live.com>
2018-10-01 22:40:05 -0400

    added image and fixed links and closed missing >

--- web/index.php
+++ web/index.php
@@ -16,12 +16,18 @@
 
 <div class="flex-container">
   <div class="box" style="background-color:#bbb">
-    <h1>About Me</h1
-<p>My name is Thomas. I actually do like to play Pokémon. I bought Diamond and Pearl for the DS when they first came out. I bought them for myself and my 2 sons. We played it a lot, hundreds of hours. On occasion my wife would let them buy packs of Pokémon cards at Walmart. The banked quite a collection of cards over time. About this time my wife hurt her neck and had to sleep in a recliner for a few months.</p>
+    <h1>About Me</h1>
+	<p>My name is Thomas. I actually do like to play Pokémon. I bought Diamond and Pearl for the DS when they first came out. I bought them for myself and my 2 sons. We played it a lot, hundreds of hours. On occasion my wife would let them buy packs of Pokémon cards at Walmart. The banked quite a collection of cards over time. About this time my wife hurt her neck and had to sleep in a recliner for a few months.</p>
 
 <p>So as not to bother her and let her rest in peace, I suggested that we try and learn of to play the card game. We did learn, but we were terrible. One day I looked online and noticed that the Pokémon World Championships were going to be played in Orlando a few weeks from then. So, we decided to go and see what that was like. We became addicted to the game. I ended up running a league for 6 years and my sons had two invites to the Pokémon World Chapionships.We stopped playing in 2014, the game format was stale and my oldest son was leaving on his mission and we were tired of traveling to tournaments all the time!</p>
+
+<h2>My favorite Card</h2>
+<img src="images/darkrai.png" alt="Darkrai Level X" width="250">
+
+
   </div>
   <div class="box" style="background-color:#ddd">
+	<h1>Time left in my job</h1>
     <p>Time left to work...</p>
   </div>
 
