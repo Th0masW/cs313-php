61a79cee36c2c9f8df5f308eac769901387a6c59
Thomas Whitelock <trnthomas@live.com>
2018-10-16 20:55:18 -0400

    annoying

--- web/Week05/index.php
+++ web/Week05/index.php
@@ -32,7 +32,7 @@ catch (PDOException $ex)
   die();
 }
 
-foreach ($db->query('SELECT * FROM state WHERE('state = "FL"')') as $row)
+foreach ($db->query('SELECT * FROM state WHERE('state = 'FL'')') as $row)
 {
   echo 'State Name: ' . $row['state'];
 
