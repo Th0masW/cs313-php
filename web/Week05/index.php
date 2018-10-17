478e15671e6c881ffc3d63dde62c858c0af83ef2
Thomas Whitelock <trnthomas@live.com>
2018-10-16 22:01:34 -0400

    wewqe

--- web/Week05/index.php
+++ web/Week05/index.php
@@ -32,7 +32,7 @@ catch (PDOException $ex)
   die();
 }
 
-foreach ($db->query('SELECT * FROM state WHERE id = 9') as $row)
+foreach ($db->query("SELECT * FROM public.state WHERE state = 'FL'") as $row)
 {
   echo 'State Name: ' . $row['state'];
 
