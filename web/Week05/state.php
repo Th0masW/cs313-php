98c5745caeb308d5c87be325b99e598c703bb1d3
Thomas Whitelock <trnthomas@live.com>
2018-10-22 08:04:39 -0400

    finish for now

--- web/Week05/state.php
+++ web/Week05/state.php
@@ -68,12 +68,6 @@ catch (PDOException $ex)
   die();
 }
 
-foreach ($db->query("SELECT * FROM public.state WHERE state = 'UT'") as $row)
-{
-  echo 'State Name: ' . $row['state'];
-
-  echo '<br/>';
-}
 ?>
 	
     <!-- Page Content -->
@@ -162,81 +156,6 @@ foreach ($db->query("SELECT * FROM public.state WHERE state = 'UT'") as $row)
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-
-<?php 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-
-foreach ($db->query("SELECT * FROM public.state WHERE state = 'UT'") as $row)
-{
-  echo 'State Name: ' . $row['state'];
-
-  echo '<br/>';
-}
-?>
-
-
-<h1>Scripture Resources</h1>
-        <ul>
-        <?php foreach ($db->query("select state.state,count(*) 
-						from state inner join annoying_people 
-						on state.id=annoying_people.state group by state.state order by count desc;") as $row): ?>
-            <li>
-                <strong>
-                    <?php echo($row["state"]); ?>  
-                    <?php echo($row["count"]); ?>
-                </strong>
-                
-            </li>
-        <?php endforeach; ?>
-        </ul>
-        <hr />
-<?php foreach ($db->query("SELECT * FROM public.state") as $row): ?>
-            <li>
-                <strong>
-                    <?php echo($row["state"]); ?>
-                </strong>
-                
-            </li>
-        <?php endforeach; ?>
-		
-		<h1>Annoying States</h1>
-		<form name="form1" id="form1" action="" method="post">
-		<select name="selectid" Id="select">
-		<option value="">--- Select ---</option>
-
-		<?php foreach ($db->query("SELECT * FROM public.state") as $row): ?>                        
-               	<option>     
-					<?php echo($row["state"]); ?>
-         		</option>                    
-        <?php endforeach; ?>
-
-		</select>
-		</form>
-		
-		
-		
-		
-		
+	
 </body>
 </html>
