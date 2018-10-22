af66d18684600fd4f39f9cc50007cd069ba3e591
Thomas Whitelock <trnthomas@live.com>
2018-10-22 07:51:17 -0400

    test

--- web/Week05/state.php
+++ web/Week05/state.php
@@ -45,6 +45,37 @@
       </div>
     </nav>
 
+	<?php 
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+foreach ($db->query("SELECT * FROM public.state WHERE state = 'UT'") as $row)
+{
+  echo 'State Name: ' . $row['state'];
+
+  echo '<br/>';
+}
+?>
+	
     <!-- Page Content -->
     <div class="container">
 
