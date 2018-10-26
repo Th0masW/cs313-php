958e1680394e20440c2007fb30d8b1b4e0a6cfd1
Thomas Whitelock <trnthomas@live.com>
2018-10-26 16:16:54 -0400

    qq

--- web/Week05/updateBusy.php
+++ web/Week05/updateBusy.php
@@ -1,3 +1,30 @@
+<?php session_start();?>
+<?php 
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>
+
+
 <?php
 /**********************************************************
 
@@ -8,8 +35,8 @@
 $busy = $_POST['howBizzy'];
 $CurrentTime = Time();
 
-require("dbConnect.php");
-$db = get_db();
+//require("dbConnect.php");
+//$db = get_db();
 try
 {
 	// Add the Scripture
