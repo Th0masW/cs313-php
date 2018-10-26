eb63ac43d639f18ab6b887bd6d2f3f7e76fc2509
Thomas Whitelock <trnthomas@live.com>
2018-10-26 16:40:08 -0400

    eff

--- web/Week05/updateBusy.php
+++ web/Week05/updateBusy.php
@@ -24,8 +24,6 @@ catch (PDOException $ex)
 
 ?>
 
-
-
 <?php
 /**********************************************************
 
@@ -37,32 +35,6 @@ $busy = $_POST['HowBusy'];
 $CurrentTime = Time();
 
 
-//require("dbConnect.php");
-//$db = get_db();
-try
-{
-	// Add the Scripture
-	// We do this by preparing the query with placeholder values
-	$query = 'INSERT INTO bizzy(time, busy) VALUES($CurrentTime, $busy)';
-	// Now we bind the values to the placeholders. This does some nice things
-	// including sanitizing the input with regard to sql commands.
-	//$statement->bindValue(':time', $CurrentTime);
-	//$statement->bindValue(':HowBusy', $busy);
-	//$statement->execute();
-	// get the new id
-	
-}
-catch (Exception $ex)
-{
-	// Please be aware that you don't want to output the Exception message in
-	// a production environment
-	echo "Error with DB. Details: $ex";
-	die();
-}
-// finally, redirect them to a new page to actually show the topics
-header("Location: index.php");
-die(); // we always include a die after redirects. In this case, there would be no
-       // harm if the user got the rest of the page, because there is nothing else
-       // but in general, there could be things after here that we don't want them
-       // to see.
-?>
+echo "hiiii";
+?> 
+
