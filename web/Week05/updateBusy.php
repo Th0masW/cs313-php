aa191f5976ff65915cde8d482956555d66c033a3
Thomas Whitelock <trnthomas@live.com>
2018-10-30 09:13:57 -0400

    efef

--- web/Week05/updateBusy.php
+++ web/Week05/updateBusy.php
@@ -105,7 +105,7 @@ $db->query("INSERT INTO bizzy (time, busy) VALUES (current_timestamp, $busy)");
 $hold = $busy;
 echo "hold value: ";
 echo $hold;
-$busy2 = $db->query("SELECT busy_types.$hold FROM busy inner join busy_types on busy.id=bizzy.Busy");
+$busy2 = $db->query("SELECT busy_types.BusyTypes WHERE busy_types.ID = $hold");
 echo $busy2;
 
 
