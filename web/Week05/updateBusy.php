a4892bf579e13016ce964cb429e8a69536abc99c
Thomas Whitelock <trnthomas@live.com>
2018-10-30 08:08:45 -0400

    ww

--- web/Week05/updateBusy.php
+++ web/Week05/updateBusy.php
@@ -100,7 +100,8 @@ $busy2 = $_POST['busytypes'];
 $CurrentTime = date('m/d/Y h:i:s a', time());;
 echo $CurrentTime; 
 echo "    ";
-echo $busy;
+echo $busy; <br>
+echo $busy2;
 
 $db->query("INSERT INTO bizzy (time, busy) VALUES (current_timestamp, $busy)");
 
