93431272c2f648bcf90dddc525f66d7536967d6f
Thomas Whitelock <trnthomas@live.com>
2018-10-31 08:03:32 -0400

    ewf

--- web/Week05/updateBusy.php
+++ web/Week05/updateBusy.php
@@ -104,9 +104,10 @@ echo $busy;
 $db->query("INSERT INTO bizzy (time, busy) VALUES (current_timestamp, $busy)");
 
 $statement = $db->query("SELECT busy_types.BusyTypes FROM busy_types WHERE busy_types.ID = $busy");
-$statement->execute();
+
 $results = $statement->fetch(PDO::FETCH_ASSOC);
-echo "    AArray: ";
+$statement->execute();
+echo "  Busy Code: ";
 echo $results;
 
 ?> 
